4d6f630e79acb0e9bc94ee0737abb294a890cd13
Kassiano <user@MacBook-Pro-de-User.local>
2021-09-16 22:23:05 -0300

    add string extentions exemples

---
+++ exemple/bin/string_exemple.dart
@@ -0,0 +1,31 @@
+import 'package:siberia/primitives/string_extentions.dart';
+
+void main(List<String> arguments) {
+
+  String? exempleString;
+
+  //orEmprty exemple
+  exempleString = null;
+  print(exempleString.orEmpty());
+
+  //toInt exemple
+  exempleString = "1";
+  int intValue = exempleString.toInt();
+  print(intValue);
+
+  //toIntOrNull exemple
+  exempleString = "value";
+  int? intNullValue = exempleString.toIntOrNull();
+  print(intNullValue);
+
+  //toDouble exemple
+  exempleString = "1";
+  double doubleValue = exempleString.toDouble();
+  print(doubleValue);
+
+  //toDoubleOrNull exemple
+  exempleString = "1";
+  double? doubleNullValue = exempleString.toDoubleOrNull();
+  print(doubleValue);
+
+}
