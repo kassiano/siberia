34c795ab7c28df6e2725de49b2950dba3f53e4b5
Kassiano <user@MacBook-Pro-de-User.local>
2021-09-16 22:25:32 -0300

    add number extentions exemple

---
+++ exemple/bin/number_exemple.dart
@@ -0,0 +1,12 @@
+import 'package:siberia/primitives/number_extentions.dart';
+
+void main(List<String> arguments) {
+
+  int? exempleInt;
+  double? exempleDouble;
+
+  //orZero exemple
+  print(exempleInt.orZero());
+  print(exempleDouble.orZero());
+
+}
