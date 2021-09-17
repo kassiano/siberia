496364c4dc898c2c182c0e997c999d3def27ed94
Kassiano <user@MacBook-Pro-de-User.local>
2021-09-16 21:53:03 -0300

    Add orZero number functions

---
+++ test/number_extentions_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:siberia/primitives/number_extentions.dart';
+
+void main() {
+
+  test('When int is null and orZero is called Then the result should 0', () {
+    int? input;
+    final result = input.orZero();
+    expect(result, 0);
+  });
+
+  test('When double is null and orZero is called Then the result should 0', () {
+    double? input;
+    final result = input.orZero();
+    expect(result, 0.0);
+  });
+
+}
